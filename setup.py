5b049159a9e79d29eea1d0d2454a2de33764159f
Simon Chapman <eatyourpeasapps@gmail.com>
2021-04-25 20:11:32 +0100

    cli folder

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = 'Calculate SDS and centiles using \
   
 setup(
         name ='rcpchgrowth-python-cli',
-        version ='1.0.2',
+        version ='1.0.3',
         author ='Simon Chapman',
         author_email ='eatyourpeasapps@gmail.com',
         url ='https://github.com/rcpch/rcpchgrowth-python-cli',
@@ -17,7 +17,7 @@ setup(
         long_description = long_description,
         long_description_content_type ="text/markdown",
         # packages = find_packages(),
-        py_modules=['cli'],
+        packages=['cli'],
         entry_points ={
             'console_scripts': [
                 'rcpchgrowth-cli = cli.__main__:methods'
