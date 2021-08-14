ffb9826f3b979619acdf8c4131c5ebd4ee9d1ff6
Simon Chapman <eatyourpeasapps@gmail.com>
2021-08-14 22:02:17 +0100

    bump12version typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = 'Calculate SDS and centiles using \
   
 setup(
         name ='rcpchgrowth-python-cli',
-        version ='1.1.0',
+        version='1.1.0',
         author ='Simon Chapman',
         author_email ='eatyourpeasapps@gmail.com',
         url ='https://github.com/rcpch/rcpchgrowth-python-cli',
